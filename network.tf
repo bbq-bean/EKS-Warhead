062fa0064a8db1200ad2c97419d59d02800da94a
Ubuntu <ubuntu@ip-172-31-10-246.us-west-1.compute.internal>
2025-03-01 00:47:40 +0000

    vpc complete

--- network.tf
+++ network.tf
@@ -2,6 +2,124 @@ resource "aws_vpc" "vpc" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   tags = {
-    Name = "vpc"
+    Name = "EKS-VPC"
   }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.0.0/20"
+  availability_zone = "us-west-1b"
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.16.0/20"
+  availability_zone = "us-west-1c"
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.32.0/20"
+  availability_zone = "us-west-1b"
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.48.0/20"
+  availability_zone = "us-west-1c"
+  tags = {
+    Name = "Public Subnet 2"
+  }
+}
+
+resource "aws_eip" "nat_eip_1" {
+}
+
+resource "aws_eip" "nat_eip_2" {
+}
+
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat_eip_1.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "NAT Gateway for AZ 1b"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat_eip_2.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+  tags = {
+    Name = "NAT Gateway for AZ 1c"
+  }
+}
+
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_nat_gateway.nat_gw_1.id
+  }
+  tags = {
+    Name = "Private with NAT GW to 1b"
+  }
+}
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_nat_gateway.nat_gw_2.id
+  }
+  tags = {
+    Name = "Private with NAT GW to 1c"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "EKS-VPC IGW"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "Public to IGW"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table_2.id
+}
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
 }
